ce24cceb1c650cba5d77badf28aa79ee24893528
donaldwonghaokien <donaldkien12@gmail.com>
2024-07-02 19:37:27 +0800

    metadata

--- src/app/meals/[mealSlug]/page.tsx
+++ src/app/meals/[mealSlug]/page.tsx
@@ -8,8 +8,21 @@ import { s3BucketName, s3BucketRegion } from "@/lib/meals";
 interface PageMealDetailsProps {
   params: {
     mealSlug: string;
-  }
+  };
 }
+
+export const generateMetadata = async ({ params }: PageMealDetailsProps) => {
+  const meal = await getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+};
 
 const PageMealDetails = async ({ params }: PageMealDetailsProps) => {
   const meal = await getMeal(params.mealSlug);
@@ -18,13 +31,17 @@ const PageMealDetails = async ({ params }: PageMealDetailsProps) => {
     notFound();
   }
 
-  meal.instructions = meal.instructions.replace(/\n/g, '<br/>')
+  meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
 
   return (
     <Fragment>
       <header className={classes.header}>
         <div className={classes.image}>
-          <Image src={`https://${s3BucketName}.s3.${s3BucketRegion}.amazonaws.com/${meal.image}`} fill alt={meal.title}/>
+          <Image
+            src={`https://${s3BucketName}.s3.${s3BucketRegion}.amazonaws.com/${meal.image}`}
+            fill
+            alt={meal.title}
+          />
         </div>
         <div className={classes.headerText}>
           <h1>{meal.title}</h1>
--- src/app/meals/page.tsx
+++ src/app/meals/page.tsx
@@ -4,6 +4,11 @@ import Link from "next/link";
 import MealsGrid from "@/components/MealsGrid/MealsGrid";
 import { getMeals } from "@/lib/meals";
 
+export const metadata = {
+  title: "All Meals",
+  description: "Browse the delicious meals shared by out vibrant community.",
+};
+
 const Meals = async (): Promise<JSX.Element> => {
   const meals = await getMeals();
   return <MealsGrid meals={meals} />;
